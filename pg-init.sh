d5c302e794900266aa70161e8ebcf36769781887
Andrej Vyšný <vysnyandrej@gmail.com>
2026-01-29 19:24:02 +0100

    merged

--- pg-init.sh
+++ pg-init.sh
@@ -10,6 +10,7 @@ set -e
 # Read passwords from environment (passed by Docker)
 N8N_PASS="${N8N_DB_PASSWORD:?N8N_DB_PASSWORD must be set}"
 POSTIZ_PASS="${POSTIZ_DB_PASSWORD:?POSTIZ_DB_PASSWORD must be set}"
+TEMPORAL_PASS="${TEMPORAL_DB_PASSWORD:-${POSTIZ_DB_PASSWORD}}"
 
 echo "Creating n8n database and user..."
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
@@ -23,6 +24,13 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE ROLE postiz LOGIN PASSWORD '$POSTIZ_PASS';
     CREATE DATABASE postiz OWNER postiz;
     GRANT ALL PRIVILEGES ON DATABASE postiz TO postiz;
+EOSQL
+
+echo "Creating temporal database and user..."
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE ROLE temporal LOGIN PASSWORD '$TEMPORAL_PASS';
+    CREATE DATABASE temporal OWNER temporal;
+    GRANT ALL PRIVILEGES ON DATABASE temporal TO temporal;
 EOSQL
 
 echo "PostgreSQL initialization complete."
